b6e6aa5bdd2ed2bdd0ffdb67a5ce43dcc9de5150
Noir <77028314+Noirscode@users.noreply.github.com>
2022-04-09 17:45:02 +0200

    fixed: metadata

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -7,23 +7,23 @@ export const Heads:FC<IProps> = function({page}){
     return (
     <Head>
         <title>Noir - {page}</title>
-        <meta name="description" content="I'm backend developer who has also experiences in frontend projects using with Next ad Tailwind" />
+        <meta name="description" content="I'm backend developer who has also experiences in frontend projects using with NextJS and Tailwindcss" />
         <link rel="icon" href="/assets/pp.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
         <meta name="title" content="Noir"/>
-        <meta name="description" content="I'm backend developer who has also experiences in frontend projects using with Next ad Tailwind"/>
+        <meta name="description" content="I'm backend developer who has also experiences in frontend projects using with NextJS and Tailwindcss"/>
 
         <meta property="og:type" content="website"/>
         <meta property="og:url" content="https://noirs.me"/>
         <meta property="og:title" content="Noir"/>
-        <meta property="og:description" content="I'm backend developer who has also experiences in frontend projects using with Next ad Tailwind"/>
+        <meta property="og:description" content="I'm backend developer who has also experiences in frontend projects using with NextJS and Tailwindcss"/>
         <meta property="og:image" content="https://noirs.me/assets/pp.png"/>
         <meta property="theme-color" content="#6366f1"/>
 
         <meta property="twitter:url" content="https://noirs.me"/>
         <meta property="twitter:title" content="noirs.me"/>
-        <meta property="twitter:description" content="I'm backend developer who has also experiences in frontend projects using with Next ad Tailwind"/>
+        <meta property="twitter:description" content="I'm backend developer who has also experiences in frontend projects using with NextJS and Tailwindcss"/>
         <meta property="twitter:image" content="https://noirs.me/assets/pp.png"/>
         <meta property="twitter:domain" content="noirs.me"/>
 
