f4201c52449c578d19296b530e56c2342560225a
Taha Kaçmaz <hi@noirs.me>
2022-09-07 23:18:01 +0300

    changed: color of notification

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,37 @@
-import type { NextPage } from 'next'
-import { About } from "@components/Home/About"
-import { Experiences } from "@components/Home/Experiences"
-import { Heads } from "@components/Head"
-import { Projects } from "@components/Home/Projects"
-import { Repositories } from "@components/Home/Repositories"
-import { ToastContainer, toast } from 'react-toastify';
-import { useEffect } from "react"
-import 'react-toastify/dist/ReactToastify.css';
+import type { NextPage } from "next";
+import { About } from "@components/Home/About";
+import { Experiences } from "@components/Home/Experiences";
+import { Heads } from "@components/Head";
+import { Projects } from "@components/Home/Projects";
+import { Repositories } from "@components/Home/Repositories";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect } from "react";
+import "react-toastify/dist/ReactToastify.css";
 const Main: NextPage = () => {
   const notify = () => toast("Please click the avatar for the page rootes");
   useEffect(() => {
-    if(typeof window != "undefined"){
+    if (typeof window != "undefined") {
       //if(!window.localStorage.getItem("Routeinfo")) {
-        toast("Please click the avatar for the page rootes");
+      toast("Please click the avatar for the page rootes", {
+        theme: "dark",
+      });
       //}
-      window.localStorage.setItem("Routeinfo", "true")
-
-    } 
-  })
+      window.localStorage.setItem("Routeinfo", "true");
+    }
+  });
   return (
     <div className="w-full h-screen">
- 
-    <div className="hidden lg:inline-block">
-    <ToastContainer />
-
-    </div>
-      <Heads page='Home'/>
-      <About/>
-      <Experiences/>
-      <Projects/>
-      <Repositories/>
+      <div className="hidden lg:inline-block">
+        <ToastContainer />
+      </div>
+      <Heads page="Home" />
+      <About />
+      <Experiences />
+      <Projects />
+      <Repositories />
       <br />
-      
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
